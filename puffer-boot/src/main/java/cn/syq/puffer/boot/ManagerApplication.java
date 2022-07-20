be8e69e1a9e287c135ff943e55cbcb96e6decce4
shiyuqin <reed2425537764@live.cn>
2022-07-20 17:41:45 +0800

    add maven dependency

--- puffer-boot/src/main/java/cn/syq/puffer/boot/ManagerApplication.java
+++ puffer-boot/src/main/java/cn/syq/puffer/boot/ManagerApplication.java
@@ -4,12 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 
 /**
  * @Author: shiyuqin
  * @Date: 2022/7/16 20:26
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedissonAutoConfiguration.class})
 @MapperScan("cn.syq.puffer.dao.sql.mapper")
 @ComponentScan({"cn.syq.puffer.manager","cn.syq.puffer.business"})
 public class ManagerApplication {
